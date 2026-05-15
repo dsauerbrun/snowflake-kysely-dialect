b9afd9c307e282c98b3784cca3e3af350b3402de
Daniel Sauerbrun <daniel.sauerbrun@vanta.com>
2026-05-15 11:59:34 -0600

    Add destroyConnection to evict poisoned connections from pool

--- src/SnowflakeDriver.ts
+++ src/SnowflakeDriver.ts
@@ -1,5 +1,5 @@
 import { type DatabaseConnection, type Driver } from 'kysely'
-import snowflake from 'snowflake-sdk'
+import snowflake, { type Connection } from 'snowflake-sdk'
 import { SnowflakeConnection } from './SnowflakeConnection.js'
 import { type SnowflakeDialectConfig } from './SnowflakeDialect.js'
 
@@ -14,7 +14,8 @@ const DEFAULT_POOL_OPTIONS: snowflake.PoolOptions = {
 export class SnowflakeDriver implements Driver {
   readonly #config: SnowflakeDialectConfig
   #pool: ReturnType<typeof snowflake.createPool> | null = null
-  #pendingReleases = new Map<SnowflakeConnection, () => void>()
+  // Tracks the underlying SDK connection for each wrapper so we can release or destroy it.
+  #resources = new Map<SnowflakeConnection, Connection>()
 
   constructor(config: SnowflakeDialectConfig) {
     this.#config = config
@@ -28,15 +29,10 @@ export class SnowflakeDriver implements Driver {
   }
 
   async acquireConnection(): Promise<DatabaseConnection> {
-    return new Promise<DatabaseConnection>((resolveAcquire, rejectAcquire) => {
-      this.#pool!.use(async (sdkConn) => {
-        const conn = new SnowflakeConnection(sdkConn)
-        return new Promise<void>((resolveRelease) => {
-          this.#pendingReleases.set(conn, resolveRelease)
-          resolveAcquire(conn)
-        })
-      }).catch(rejectAcquire)
-    })
+    const sdkConn = await this.#pool!.acquire()
+    const conn = new SnowflakeConnection(sdkConn)
+    this.#resources.set(conn, sdkConn)
+    return conn
   }
 
   async beginTransaction(conn: SnowflakeConnection): Promise<void> {
@@ -52,10 +48,18 @@ export class SnowflakeDriver implements Driver {
   }
 
   async releaseConnection(conn: SnowflakeConnection): Promise<void> {
-    const release = this.#pendingReleases.get(conn)
-    if (release) {
-      this.#pendingReleases.delete(conn)
-      release()
+    const sdkConn = this.#resources.get(conn)
+    if (sdkConn) {
+      this.#resources.delete(conn)
+      await this.#pool!.release(sdkConn)
+    }
+  }
+
+  async destroyConnection(conn: SnowflakeConnection): Promise<void> {
+    const sdkConn = this.#resources.get(conn)
+    if (sdkConn) {
+      this.#resources.delete(conn)
+      await this.#pool!.destroy(sdkConn)
     }
   }
 
